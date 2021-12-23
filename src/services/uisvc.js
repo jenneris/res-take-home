154fcab9f43db37f4f220b1b7cbea2c957cc3c43
Jennifer Everhart <jenn@jennifers-mbp.lan>
2021-12-23 14:41:29 -0500

    cleanup a bit

--- src/services/uisvc.js
+++ src/services/uisvc.js
@@ -18,21 +18,7 @@ export const clearNotifications = async (notifications) => {
     const baseUrl = ENV[process.env.REACT_APP_DEV_ENV].BASE_URL;
     const responses = [];
     notifications.forEach(async function (notificaton) {
-        console.log(notificaton.id)
-        // var raw = "";
-
-        // var requestOptions = {
-        //   method: 'DELETE',
-        //   body: raw,
-        //   redirect: 'follow',
-        //   headers: new Headers({'Access-Control-Allow-Origin': '*'}),
-
-        // };
-        
-        // fetch(`http://localhost:3001/api/notification/${notificaton.id}`, requestOptions)
-        //   .then(response => response.text())
-        //   .then(result => console.log(result))
-        //   .catch(error => console.log('error', error));
+    console.log(notificaton.id)
     const response = await fetch('http://localhost:3001/api/notification?id=21', { method: 'DELETE' });
     const data = await response.json();
     responses.push(data);
