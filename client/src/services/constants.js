a7a94e75047d0849252cd275c1ce2c5a81e5508c
Jennifer Everhart <jenn@jennifers-mbp.lan>
2021-12-26 03:01:45 -0500

    trying to have react and server on heroku

--- client/src/services/constants.js
+++ client/src/services/constants.js
@@ -4,6 +4,9 @@ export const ENV = {
     }, 
     "DEV":{
         'BASE_URL':'https://jenn-test-app.herokuapp.com:3001/api/',
+    },
+    "STAGING":{
+        'BASE_URL':'https://platform-notification-service.herokuapp.com/api/',
     }
 };
 export const NOTIFICATIONS_URL = "notification";
